c708520c67f2d95a721e2a22243285cbc1bd24a6
Utkarsh Vishwakarma <utkarshrkt2001@gmail.com>
2022-09-13 10:22:22 +0530

    set headers for cors

--- server.js
+++ server.js
@@ -17,10 +17,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(function (req, res, next) {
   res.header(" Access-Control-Allow-Origin ", " * ");
-  res.header(
-    " Access-Control-Allow-Headers ",
-    " Origin , X-Requested-with , Content-Type , Accept "
-  );
   next();
 });
 
